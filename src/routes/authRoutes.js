d9eb1d5b5d3488bb097896fda70bf619e7295bfa
Roufth <ridhoalfathnusantara@example.com>
2024-11-23 18:11:30 +0700

    signup & auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import AuthController from '../controllers/authController.js';  
 
-const authRouter = express.Router();
+const router = express.Router();
 
-authRouter.post('/signup', AuthController.signup);  
+router.post('/signup', AuthController.signup);  
 
-export default authRouter;
+export default router;
